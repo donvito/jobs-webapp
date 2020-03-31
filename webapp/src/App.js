d61e6ca44d365ca296e67007461e98c1e0dd48ee
Melvin Vivas <melvindave@gmail.com>
2020-03-31 20:51:29 +0800

    minor design updates

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -36,47 +36,31 @@ function App() {
   }, [])
 
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={broken => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-          <Menu.Item key="1">
-            <UserOutlined />
-            <span className="nav-text"><a href="https://www.melvinvivas.com">By Melvin Vivas</a></span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            {/* <h3 style={{ margin: '24px 0' }}>Golang Jobs in Singapore</h3> */}
-            <Title level={2}>Golang Jobs in Singapore</Title>
-              Sources: Indeed, LinkedIn, JobsDB
-              <Search
-              placeholder="Job Title, Company or Description"
-              enterButton="Search"
-              size="large"
-              onSearch={value => {
-                console.log(value)
 
-                fetch('https://api.rs2.usw2.rockset.com/v1/orgs/self/ws/jobportal/lambdas/search/versions/6',
-                  {
-                    method: "POST",
-                    headers: new Headers({
-                      Authorization: 'ApiKey ' + process.env.REACT_APP_ROCKSET_API_KEY,
-                      'Content-Type': 'application/json'
-                    }),
-                    body: `{
+    <Layout>
+      {/* <Header className="site-layout-sub-header-background" style={{ padding: 20, position: 'fixed', zIndex: 1, width: '100%' }} /> */}
+
+      <Content style={{ margin: '24px 80px 0' }}>
+        <div className="site-layout-background" style={{ padding: 0, minHeight: 360 }}>
+          {/* <h3 style={{ margin: '24px 0' }}>Golang Jobs in Singapore</h3> */}
+          
+          <Title level={2} style={{ textAlign: 'center' }}><img src="Go-Logo_Fuchsia.png" alt="Logo" width="150px" height="120px"/><br/>Golang Jobs (Singapore)</Title>
+              
+              <Search
+            placeholder="Job Title, Company or Description"
+            enterButton="Search"
+            size="large"
+            onSearch={value => {
+              console.log(value)
+
+              fetch('https://api.rs2.usw2.rockset.com/v1/orgs/self/ws/jobportal/lambdas/search/versions/6',
+                {
+                  method: "POST",
+                  headers: new Headers({
+                    Authorization: 'ApiKey ' + process.env.REACT_APP_ROCKSET_API_KEY,
+                    'Content-Type': 'application/json'
+                  }),
+                  body: `{
                       "parameters": [
                         {
                           "name": "searchstring",
@@ -85,42 +69,43 @@ function App() {
                         }
                       ]
                     }`
-                  })
-                  .then(response => response.json())
-                  .then(result => {
-                    //console.log(JSON.stringify(result))
-                    setJobs(result.results)
+                })
+                .then(response => response.json())
+                .then(result => {
+                  //console.log(JSON.stringify(result))
+                  setJobs(result.results)
 
-                  })
-                  .catch(error => console.log(error));
-              }
+                })
+                .catch(error => console.log(error));
+            }
 
-              }
-            /><br/><br/>
-            <Text strong>Search results { jobs.length } jobs</Text><br/>
-            <List
-              itemLayout="horizontal"
-              dataSource={jobs}
-              renderItem={item => (
-                <List.Item>
-                  <List.Item.Meta
-                    // avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                    title={<span><a href={item.JobUrl}><Title level={4}>{item.Title}</Title></a><Text strong>{item.Company}</Text></span>}
-                  // description={item.Company}
-                  />
-                  {item.Summary}<br />
-                  {/* <Text strong>Source: {item.Source}</Text>  */}
-                  {<div><Text code>Last Updated: <Moment>{item.EventTime}</Moment></Text></div>}
+            }
+          /><br /><br />
+          <Text style={{ textAlign: 'center' }}>Sources: Indeed, LinkedIn, JobsDB</Text><br /><br />
+          <Text strong>Search results {jobs.length} jobs</Text><br />
+          <List
+            itemLayout="horizontal"
+            dataSource={jobs}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  // avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                  title={<span><a href={item.JobUrl}><Title level={4}>{item.Title}</Title></a><Text strong>{item.Company}</Text></span>}
+                // description={item.Company}
+                />
+                {item.Summary}<br />
+                {/* <Text strong>Source: {item.Source}</Text>  */}
+                {<div><Text code>Last Updated: <Moment>{item.EventTime}</Moment></Text></div>}
 
 
-                </List.Item>
-              )}
-            />
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Jobs Search</Footer>
-      </Layout>
+              </List.Item>
+            )}
+          />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Jobs Search</Footer>
     </Layout>
+
 
   );
 }
